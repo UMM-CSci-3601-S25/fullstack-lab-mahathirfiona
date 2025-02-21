a3c27f417d219e5b01192cc51d9133b2cf97265d
Mahathir <mosta044@umn.edu>
2025-02-21 14:30:56 -0600

    Add todo
    Spec testing and styling file

--- client/src/app/todos/add-todo.component.spec.ts
+++ client/src/app/todos/add-todo.component.spec.ts
@@ -323,21 +323,23 @@ describe('AddTodoComponent#submitForm()', () => {
   // error case doesn't navigate to another page. It just displays an error
   // message in the snackbar. So, we don't need to worry about the asynchronous
   // nature of navigation.
-  it('should call addUser() and handle error response', () => {
+  it('should call addTodo() and handle error response', () => {
     // Save the original path so we can check that it doesn't change.
     const path = location.path();
     // A canned error response to be returned by the spy.
     const errorResponse = { status: 500, message: 'Server error' };
-    // "Spy" on the `.addUser()` method in the user service. Here we basically
+    // "Spy" on the `.addTodo()` method in the Todo service. Here we basically
     // intercept any calls to that method and return the error response
     // defined above.
-    const addUserSpy = spyOn(todoService, 'addTodo')
+    const addTodoSpy = spyOn(todoService, 'addTodo')
       .and
       .returnValue(throwError(() => errorResponse));
     component.submitForm();
-    // Check that `.addUser()` was called with the form's values which we set
+    // Check that `.addTodo()` was called with the form's values which we set
     // up above.
     expect(addTodoSpy ).toHaveBeenCalledWith(component.addTodoForm.value);
+
+
     // Confirm that we're still at the same path.
     expect(location.path()).toBe(path);
   });
