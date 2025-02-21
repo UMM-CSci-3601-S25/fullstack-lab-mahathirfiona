f7e6eda9136d071246f00c152bd5326a6c7c0281
Mahathir <mosta044@umn.edu>
2025-02-20 23:36:48 -0600

    Committing Add-todo components
    Created and edited add-todo components to the client code.

--- client/src/app/todos/add-todo.component.ts
+++ client/src/app/todos/add-todo.component.ts
@@ -8,107 +8,73 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { UserRole } from './user';
-import { UserService } from './user.service';
+import { Todo, TodoCategory } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
-    selector: 'app-add-user',
-    templateUrl: './add-user.component.html',
-    styleUrls: ['./add-user.component.scss'],
+    selector: 'app-add-todo',
+    templateUrl: './add-todo.component.html',
+    styleUrls: ['./add-todo.component.scss'],
     imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule]
 })
-export class AddUserComponent {
+export class AddTodoComponent {
 
-  addUserForm = new FormGroup({
-    // We allow alphanumeric input and limit the length for name.
-    name: new FormControl('', Validators.compose([
+  addTodoForm = new FormGroup({
+    owner: new FormControl('', Validators.compose([
       Validators.required,
       Validators.minLength(2),
-      // In the real world you'd want to be very careful about having
-      // an upper limit like this because people can sometimes have
-      // very long names. This demonstrates that it's possible, though,
-      // to have maximum length limits.
       Validators.maxLength(50),
-      (fc) => {
-        if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
-          return ({existingName: true});
-        } else {
-          return null;
-        }
-      },
     ])),
 
-    // Since this is for a company, we need workers to be old enough to work, and probably not older than 200.
-    age: new FormControl<number>(null, Validators.compose([
+    body: new FormControl(''),
+
+    status: new FormControl('Incomplete', Validators.compose([
       Validators.required,
-      Validators.min(15),
-      Validators.max(200),
-      // In the HTML, we set type="number" on this field. That guarantees that the value of this field is numeric,
-      // but not that it's a whole number. (The user could still type -27.3232, for example.) So, we also need
-      // to include this pattern.
-      Validators.pattern('^[0-9]+$')
+      Validators.pattern('^(Complete|Incomplete)$'),
     ])),
 
-    // We don't care much about what is in the company field, so we just add it here as part of the form
-    // without any particular validation.
-    company: new FormControl(''),
-
-    // We don't need a special validator just for our app here, but there is a default one for email.
-    // We will require the email, though.
-    email: new FormControl('', Validators.compose([
+    category: new FormControl<TodoCategory>('video games', Validators.compose([
       Validators.required,
-      Validators.email,
-    ])),
-
-    role: new FormControl<UserRole>('viewer', Validators.compose([
-      Validators.required,
-      Validators.pattern('^(admin|editor|viewer)$'),
+      Validators.pattern('^(homework|groceries|software design|video games)$'),
     ])),
   });
 
 
   // We can only display one error at a time,
   // the order the messages are defined in is the order they will display in.
-  readonly addUserValidationMessages = {
-    name: [
-      {type: 'required', message: 'Name is required'},
-      {type: 'minlength', message: 'Name must be at least 2 characters long'},
-      {type: 'maxlength', message: 'Name cannot be more than 50 characters long'},
-      {type: 'existingName', message: 'Name has already been taken'}
+  readonly addTodoValidationMessages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
     ],
 
-    age: [
-      {type: 'required', message: 'Age is required'},
-      {type: 'min', message: 'Age must be at least 15'},
-      {type: 'max', message: 'Age may not be greater than 200'},
-      {type: 'pattern', message: 'Age must be a whole number'}
+    category: [
+      { type: 'required', message: 'Category is required' },
+      { type: 'pattern', message: 'Category must be Homework, Groceries, Software Design, and Video Games' },
     ],
 
-    email: [
-      {type: 'email', message: 'Email must be formatted properly'},
-      {type: 'required', message: 'Email is required'}
-    ],
-
-    role: [
-      { type: 'required', message: 'Role is required' },
-      { type: 'pattern', message: 'Role must be Admin, Editor, or Viewer' },
+      body: [
+        { type: 'required', message: 'body is required' },
+        { type: 'minlength', message: 'body must be at least 4 characters long'},
+  
     ]
   };
 
   constructor(
-    private userService: UserService,
+    private todoService: TodoService,
     private snackBar: MatSnackBar,
     private router: Router) {
   }
 
-  formControlHasError(controlName: string): boolean {
-    return this.addUserForm.get(controlName).invalid &&
-      (this.addUserForm.get(controlName).dirty || this.addUserForm.get(controlName).touched);
+  formControlHasError(controlOwner: string): boolean {
+    return this.addTodoForm.get(controlOwner).invalid &&
+      (this.addTodoForm.get(controlOwner).dirty || this.addTodoForm.get(controlOwner).touched);
   }
 
-  getErrorMessage(name: keyof typeof this.addUserValidationMessages): string {
-    for(const {type, message} of this.addUserValidationMessages[name]) {
-      if (this.addUserForm.get(name).hasError(type)) {
+  getErrorMessage(owner: keyof typeof this.addTodoValidationMessages): string {
+    for(const {type, message} of this.addTodoValidationMessages[owner]) {
+      if (this.addTodoForm.get(owner).hasError(type)) {
         return message;
       }
     }
@@ -116,25 +82,26 @@ export class AddUserComponent {
   }
 
   submitForm() {
-    this.userService.addUser(this.addUserForm.value).subscribe({
+    const newTodo: Partial<Todo> = { owner: this.addTodoForm.value.owner, status: this.addTodoForm.value.status === 'Complete', category: this.addTodoForm.value.category, body: this.addTodoForm.value.body};
+    this.todoService.addTodo(newTodo).subscribe({
       next: (newId) => {
         this.snackBar.open(
-          `Added user ${this.addUserForm.value.name}`,
+          `Added todo ${this.addTodoForm.value.body}`,
           null,
           { duration: 2000 }
         );
-        this.router.navigate(['/users/', newId]);
+        this.router.navigate(['/todos/', newId]);
       },
       error: err => {
         if (err.status === 400) {
           this.snackBar.open(
-            `Tried to add an illegal new user – Error Code: ${err.status}\nMessage: ${err.message}`,
+            `Tried to add an illegal new todo – Error Code: ${err.status}\nMessage: ${err.message}`,
             'OK',
             { duration: 5000 }
           );
         } else if (err.status === 500) {
           this.snackBar.open(
-            `The server failed to process your request to add a new user. Is the server up? – Error Code: ${err.status}\nMessage: ${err.message}`,
+            `The server failed to process your request to add a new todo. Is the server up? – Error Code: ${err.status}\nMessage: ${err.message}`,
             'OK',
             { duration: 5000 }
           );
